264880f830d3520a4996b9c43ea5502b72aa4f9c
John Kelleher <johnpkelleher@gmail.com>
2018-10-25 12:01:50 -0400

    full ropsten deployment

--- truffle.js
+++ truffle.js
@@ -1,11 +1,15 @@
+
 require('babel-register')
 require('babel-polyfill')
+
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const secrets = require('./secrets.json');
 
 module.exports = {
   networks: {
     development: {
       host: 'localhost',
-      port: 8546,
+      port: 8545,
       network_id: '*', // Match any network id
       gas: 6000000,
       gasPrice: 0x01
@@ -16,6 +20,12 @@ module.exports = {
       port: 8546,
       gas: 0xfffffffffff,
       gasPrice: 0x01
-    }
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(secrets.mnemonic, "https://ropsten.infura.io/<secrets.infura_apikey>")
+      },
+      network_id: 3
+    }   
   }
 }
